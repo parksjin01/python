a8b00a05a0d60c5227ffa47bd8809612b0f72e1f
parksjin01 <parksjin01@gmail.com>
2016-03-21 10:27:19 +0900

    Never Search Program
    ...

--- test.py
+++ test.py
@@ -1,57 +1,29 @@
-#-*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import urllib2
-import re
-import sys
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from PyQt4 import uic
+import webbrowser
+site= "https://search.naver.com/search.naver?where=nexearch&query=zz&sm=top_hty&fbm=0&ie=utf8"
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = urllib2.Request(site,headers=hdr)
+response = urllib2.urlopen(req)
+print 'Response: ', response
+print 'URL: ', response.geturl()
 
-form_class = uic.loadUiType("naver.ui")[0]
+"""headers=response.info()
+print 'Date: ', headers['date']
+print 'Headers: '
+print '-'*50
+print headers
 
-class MyWindow(QMainWindow, form_class):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setupUi(self)
-        self.connect(self.pushButton, SIGNAL("clicked()"), self.btn_clicked)
+dat=response.read(100000)
+print 'Length: ', len(dat)
+print 'Data: '
+print '-'*50
+print dat"""
 
-    def refresh(self):
-        self.response=urllib2.urlopen('http://www.naver.com')
-        self.tmp=[]
-        dat=self.response.read()
-        r=re.search('noscript', dat)
-        print r
-        s=r.start()
-        r=re.search('/noscript', dat)
-        e=r.end()
-        dat= dat[s:e + 1].split('\n')
-        for i in range(4, 14):
-            s=re.search('">', dat[i]).end()
-            e=re.search('</', dat[i]).start()
-            self.tmp.append((dat[i][s:e]))
+# Open URL in new browser window
 
-        for i in self.tmp:
-            print i
-        return self.tmp
+# Opens in safari browser
 
-    def btn_clicked(self):
-        self.refresh()
-        print self.tmp
-        self.label_11.setText(unicode(self.tmp[0]))
-        self.label_12.setText(unicode(self.tmp[1]))
-        self.label_13.setText(unicode(self.tmp[2]))
-        self.label_14.setText(unicode(self.tmp[3]))
-        self.label_15.setText(unicode(self.tmp[4]))
-        self.label_16.setText(unicode(self.tmp[5]))
-        self.label_17.setText(unicode(self.tmp[6]))
-        self.label_18.setText(unicode(self.tmp[7]))
-        self.label_19.setText(unicode(self.tmp[8]))
-        self.label_20.setText(unicode(self.tmp[9]))
+# Open URL in a new tab
+webbrowser.open_new_tab(response.geturl()) # opens in default browser
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    myWindow = MyWindow()
-    myWindow.show()
-    app.exec_()
+# Opens in safari browser
