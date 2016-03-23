7afec9e2fcfba9615a9abf28d26d38ecc88f774d
parksjin01 <parksjin01@gmail.com>
2016-03-23 22:36:58 +0900

    나만의 쉘
    ...

--- mod1.py
+++ mod1.py
@@ -1,7 +1,15 @@
+#-*- coding: utf-8 -*-
+import os
+import subprocess
 import sys
-from PyQt4.QtGui import *
+reload(sys)
+sys.setdefaultencoding('utf-8')
+cmd=[]
+c=''
 
-app = QApplication(sys.argv)
-label = QLabel("Hello PyQt")
-label.show()
-app.exec_()
+while True:
+    cmd=raw_input()
+    pmd=subprocess.check_output(c+cmd+';pwd', shell=True).split('\n')
+    for i in pmd:
+        print i;
+    c='cd '+''.join(pmd[-2])+';'
