27e71751d67e75ab336e11600b543e93e6ab70ca
parksjin01 <parksjin01@gmail.com>
2016-05-04 17:52:18 +0900

    Added files via upload

--- practice.py
+++ practice.py
@@ -1,44 +1,32 @@
-from BaseHTTPServer import BaseHTTPRequestHandler
-import urlparse
+fileName=raw_input()
+key=input()
+oper=raw_input()
+pw=input()
+tmp=0
+filePass=open("res.txt", "r")
+if(pw==123654):
+    files=open(fileName, "rb")
+    tmp=list(files.read())
+    #print tmp
+    files.close()
+    files=open(fileName, "wb")
+    files.write('')
+    files.close()
+    files=open(fileName, "ab")
+    if oper=="d":
+        for i in tmp:
+            if ord(i)-key>=0:
+                files.write(chr(ord(i)-key))
+            else:
+                files.write(chr(ord(i)-key+256))
 
-class GetHandler(BaseHTTPRequestHandler):
+    elif oper=="e":
+        #print tmp
+        for i in tmp:
+            if ord(i)+key<256:
+                files.write(chr(ord(i)+key))
+            else:
+                files.write(chr(ord(i)+key-256))
 
-    def do_GET(self):
-        parsed_path=urlparse.urlparse(self.path)
-        message_parts=[
-            'Client Values:',
-            'client_address=%s (%s)' %(self.client_address, self.address_string()),
-            'command=%s' %self.command,
-            'path=%s' %self.path,
-            'real path=%s' %parsed_path.path,
-            'query=%s' %parsed_path.query,
-            'request version=%s' %self.request_version,
-            '',
-            'Server Values:',
-            'server version=%s' %self.server_version,
-            'sys_version=%s' %self.sys_version,
-            'protocol_version=%s' %self.protocol_version,
-            '',
-            'HEADERS RECIEVED',
-        ]
-
-        """message_parts=[
-            '<a href="http://www.naver.com">Naver</a>',
-            '<a href="http://www.google.com">Google</a>',
-            '<a href="http://www.gachon.ac.kr">Gachon</a>'
-        ]"""
-
-        for name, value in sorted(self.headers.items()):
-            message_parts.append('%s-%s' %(name, value.rstrip()))
-            message='\r\n'.join(message_parts)
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(message)
-            return
-
-
-if __name__=='__main__':
-    from BaseHTTPServer import HTTPServer
-    server=HTTPServer(('localhost', 8080), GetHandler)
-    print 'Starting server'
-    server.serve_forever()
+    else:
+        print "Error"
